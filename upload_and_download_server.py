550a2692571e42ef94991673fcb79de8e8c9d3cd
TaehyeonPark <84885011+TaehyeonPark@users.noreply.github.com>
2022-12-26 22:26:43 +0900

    Create upload_and_download_server.py

---
+++ upload_and_download_server.py
@@ -0,0 +1,38 @@
+from flask import *
+from flask_compress import *
+from flask_cors import *
+import os
+
+app = Flask(__name__, template_folder='viewer')
+app.secret_key = os.urandom(24)
+Compress(app)
+CORS(app)
+path = "/backups"
+destination="/download/"
+
+@app.route('/')
+def index():
+    return render_template('upload_and_download_page.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    files = []
+    for i in request.files:
+        file = request.files[i]
+        file.save(destination + os.path.join(file.filename))
+    return 'ok'
+
+@app.route('/download', methods=['POST', 'GET'])
+def download():
+    file = request.args.get('file')
+    dir = os.listdir(path)
+    if file == None:
+        return {'files': dir}
+    else:
+        if file in dir:
+            return send_file(path + '/{}'.format(file))
+        else:
+            return {'files': dir}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
