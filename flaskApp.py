5772267231e8caf1726ffee8fff20b9f6a3cd0ac
TaehyeonPark <84885011+TaehyeonPark@users.noreply.github.com>
2023-01-06 02:13:41 +0900

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -36,4 +36,4 @@ def download():
             return {'files': dir}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
