e655611c1979c48ebf59c9ea60fd873d050da5c8
TaehyeonPark <84885011+TaehyeonPark@users.noreply.github.com>
2023-01-22 17:33:32 +0900

    Delete certbot.sh

--- certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FULLDOMAIN=''
-EMAIL='op@taehyeon.me'
-echo "email(=op@taehyeon.me)"
-echo "Enter fulldomain"
-read FULLDOMAIN
-
-certbot certonly --webroot --agree-tos --email $EMAIL -d $FULLDOMAIN -w /var/www/html/
